80387a16e554fdf5e75672eea8f7d0dacfbfddba
Mayhem <alexandersaca3@gmail.com>
2026-04-09 18:35:56 -0500

    Intento de añadido de modo alma
    Aún en progreso

--- Assets/_Project/Scripts/Characters/Player_Movement.cs
+++ Assets/_Project/Scripts/Characters/Player_Movement.cs
@@ -7,6 +7,7 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] float walkSpeed = 15;
     [SerializeField] float gravity = -10;
     [SerializeField] Transform orientation;
+    [SerializeField] bool flyMode = false;
     CharacterController controller;
     Vector3 movement;
     private void Awake()
@@ -27,6 +28,12 @@ public class Player_Movement : MonoBehaviour
 
         Vector3 direction = orientation.forward * movement.z + orientation.right * movement.x;
 
+        if (flyMode)
+        {
+            Vector3 dirY = direction + orientation.up * movement.y;
+        }
+
         controller.Move((direction.normalized * walkSpeed + Vector3.up * movement.y) * Time.deltaTime);
+
     }
 }
