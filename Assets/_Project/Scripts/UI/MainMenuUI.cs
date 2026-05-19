8152b731a4f6259c48cdfba8d951f3af29da7255
Mayhem <alexandersaca3@gmail.com>
2026-05-18 19:53:10 -0500

    (W.i.P) Implementación del menú principal

---
+++ Assets/_Project/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Menu Settings")]
+    [SerializeField] string playSceneName;
+    [SerializeField] Button playButton;
+    [SerializeField] Button exitButton;
+
+    void Awake()
+    {
+        if (playButton != null) playButton.onClick.AddListener(() => ChangePlayScene(playSceneName));
+        if (exitButton != null) exitButton.onClick.AddListener(() => ExitGame());
+    }
+    void ChangePlayScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
