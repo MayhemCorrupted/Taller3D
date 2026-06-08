7720093d6d8ae7f1481ad64c1f7720545b80524d
Mayhem <alexandersaca3@gmail.com>
2026-06-08 14:15:00 -0500

    Caja fuerte

---
+++ Assets/_Project/Scripts/Core/Puzzle/SequencePathPuzzle.cs
@@ -0,0 +1,202 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class SequencePathPuzzle : MonoBehaviour, IInteractable
+{
+    [Header("UI Panel & Interaction")]
+    [SerializeField] string interactPrompt = "[E] Interact";
+    [SerializeField] GameObject uiPanel;
+    [SerializeField] Button exitButton;
+
+    [Header("Puzzle Components")]
+    [SerializeField] Button[] numpadButtons;
+    [SerializeField] Image[] sequenceLights;
+    [SerializeField] Sprite[] sequenceIcons;
+
+    [Header("Feedback Colors")]
+    [SerializeField] Color lightOffColor = Color.black;
+    [SerializeField] Color lightCorrectColor = Color.green;
+    [SerializeField] Color lightErrorColor = Color.red;
+
+    [Header("Events")]
+    [SerializeField] UnityEvent OnSolved;
+
+    int[] proceduralButtonValues;
+    int expectedNextValue = 0;
+    int currentSequenceStep = 0;
+    bool isLockedOut = false;
+    bool isSolved = false;
+    bool isUIOpen = false;
+    public string PuzzlePrompt => interactPrompt;
+   void Start()
+    {
+        if (uiPanel != null) uiPanel.SetActive(false);
+        if (exitButton != null) exitButton.onClick.AddListener(() => TogglePanel(false));
+
+        UserInterfaceManager.Instance.RegisterPanel(UserInterfaceManager.PanelType.Puzzle, () => TogglePanel(true));
+
+        InitializeProceduralGrid();
+        AssignButtonListeners();
+        ResetLights();
+    }
+
+    private void InitializeProceduralGrid()
+    {
+        Random.InitState(ProceduralSeedGenerator.Instance.SafeSeed + gameObject.name.GetHashCode());
+
+        int totalButtons = numpadButtons.Length;
+        proceduralButtonValues = new int[totalButtons];
+
+        for (int i = 0; i < totalButtons; i++)
+        {
+            proceduralButtonValues[i] = i + 1;
+        }
+
+        for (int i = 0; i < totalButtons; i++)
+        {
+            int randomIndex = Random.Range(i, totalButtons);
+            int temp = proceduralButtonValues[i];
+
+            proceduralButtonValues[i] = proceduralButtonValues[randomIndex];
+            proceduralButtonValues[randomIndex] = temp;
+        }
+
+        for (int i = 0; i < totalButtons; i++)
+        {
+            if (numpadButtons[i] != null)
+            {
+                if (numpadButtons[i].transform.childCount > 0)
+                {
+                    Image iconImage = numpadButtons[i].transform.GetChild(0).GetComponent<Image>();
+                    
+                    if (iconImage != null)
+                    {
+                        int spriteIndex = proceduralButtonValues[i] - 1; 
+                        
+                        if (spriteIndex >= 0 && spriteIndex < sequenceIcons.Length)
+                        {
+                            iconImage.sprite = sequenceIcons[spriteIndex];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void AssignButtonListeners()
+    {
+        for (int i = 0; i < numpadButtons.Length; i++)
+        {
+            int buttonIndex = i;
+            if (numpadButtons[i] != null)
+            {
+                numpadButtons[i].onClick.AddListener(() => OnButtonPressed(buttonIndex));
+            }
+        }
+    }
+
+    public string GetTextInteract() => interactPrompt;
+
+    public void Interact(Transform interactorTransform)
+    {
+        if (isSolved) return;
+        TogglePanel(!isUIOpen);
+    }
+
+    private void TogglePanel(bool state)
+    {
+        isUIOpen = state;
+
+        if (state)
+        {
+            if (!UserInterfaceManager.Instance.RequestOpenPanel(UserInterfaceManager.PanelType.Puzzle)) return;
+        }
+        else
+        {
+            UserInterfaceManager.Instance.ReportClosedPanel(UserInterfaceManager.PanelType.Puzzle);
+        }
+
+        if (uiPanel != null) uiPanel.SetActive(state);
+    }
+
+    private void OnButtonPressed(int buttonIndex)
+    {
+        if (isLockedOut || isSolved) return;
+
+        int pressedValue = proceduralButtonValues[buttonIndex];
+
+        if (currentSequenceStep == 0 || pressedValue == expectedNextValue)
+        {
+            ProcessCorrectPress(buttonIndex, pressedValue);
+        }
+        else
+        {
+            ProcessError();
+        }
+    }
+
+    private void ProcessCorrectPress(int buttonIndex, int pressedValue)
+    {
+        numpadButtons[buttonIndex].interactable = false;
+
+        if (currentSequenceStep < sequenceLights.Length && sequenceLights[currentSequenceStep] != null)
+        {
+            sequenceLights[currentSequenceStep].color = lightCorrectColor;
+        }
+
+        expectedNextValue = (pressedValue % numpadButtons.Length) + 1;
+        currentSequenceStep++;
+
+        if (currentSequenceStep >= numpadButtons.Length)
+        {
+            CompletePuzzle();
+        }
+    }
+
+    private void ProcessError()
+    {
+        StartCoroutine(ErrorResetRoutine());
+    }
+
+    private IEnumerator ErrorResetRoutine()
+    {
+        isLockedOut = true;
+
+        if (currentSequenceStep < sequenceLights.Length && sequenceLights[currentSequenceStep] != null)
+        {
+            sequenceLights[currentSequenceStep].color = lightErrorColor;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        currentSequenceStep = 0;
+        expectedNextValue = 0;
+        
+        ResetLights();
+
+        foreach (Button btn in numpadButtons)
+        {
+            if (btn != null) btn.interactable = true;
+        }
+
+        isLockedOut = false;
+    }
+
+    private void ResetLights()
+    {
+        foreach (Image light in sequenceLights)
+        {
+            if (light != null) light.color = lightOffColor;
+        }
+    }
+
+    private void CompletePuzzle()
+    {
+        isSolved = true;
+        interactPrompt = string.Empty;
+        Debug.Log("[Puzzle Caja Fuerte] Secuencia completada con éxito.");
+        OnSolved?.Invoke();
+        TogglePanel(false);
+    }
+}
