693ec683bf3237c364f55d8d9e3ea335b5928a10
LeoGPrado <leogprado28@gmail.com>
2026-07-08 11:49:26 -0500

    Secuencia de escenas

--- Assets/_Project/Scripts/UI/MainMenuUI.cs
+++ Assets/_Project/Scripts/UI/MainMenuUI.cs
@@ -1,3 +1,4 @@
+using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,19 +7,27 @@ public class MainMenuUI : MonoBehaviour
 {
     [Header("Menu Settings")]
     [SerializeField] string playSceneName;
+    [SerializeField] string creditsSceneName;
     [SerializeField] Button playButton;
     [SerializeField] Button exitButton;
+    [SerializeField] Button creditsButton;
+
     void Awake()
     {
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         if (playButton != null) playButton.onClick.AddListener(() => ChangePlayScene(playSceneName));
+        if (creditsButton != null) creditsButton.onClick.AddListener(() => ShowCredits());
         if (exitButton != null) exitButton.onClick.AddListener(() => ExitGame());
     }
     void ChangePlayScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    void ShowCredits()
+    {
+        SceneManager.LoadScene(creditsSceneName);
     }
     void ExitGame()
     {
